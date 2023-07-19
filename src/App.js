51dc6615f73b654fea2c47756394bfe5e51e31fd
jihye0 <llimjihye@gmail.com>
2023-07-19 10:33:22 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props){
+  return <header>
+    <h1>
+      <a>{props.title}</a>
+    </h1>
+  </header>
+
+}
+
 function App() {
+  const topics=[
+    {},
+    {},
+    {}
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="WEB">
+      </Header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+          <li><a href="/read/3">js</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
