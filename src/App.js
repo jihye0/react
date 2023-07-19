52124ee64f100cf2c390230a6eaba8c4af3f1362
jihye0 <llimjihye@gmail.com>
2023-07-19 10:52:54 +0900

    add event

--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Article(props){
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function Header(props){
   return <header>
     <h1>
-      <a>{props.title}</a>
+      <a href="/" onClick={(event)=>{
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a>
     </h1>
   </header>
+}
 
+function Nav(props){
+  const lis=[]
+  for (let i=0; i<props.topics.length; i++){
+    let t=props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
 }
 
 function App() {
   const topics=[
-    {},
-    {},
-    {}
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
   ]
 
   return (
     <div>
-      <Header title="WEB">
+      <Header title="WEB" onChangeMode={()=>{
+        alert('Header');
+      }}>
       </Header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Nav topics={topics} onChangeMode={(id)=>{alert(id);}}>
+      </Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
