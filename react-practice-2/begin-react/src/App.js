5aa07f83607df87da4c90a21f0b4894e50c4a376
jihye0 <llimjihye@gmail.com>
2023-07-21 18:27:16 +0900

    Until 8

--- react-practice-2/begin-react/src/App.js
+++ react-practice-2/begin-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
+import InputSample from './InputSample'
 import './App.css';
 
 function App(){
@@ -13,7 +14,7 @@ function App(){
     padding: '1rem'
   }
   return (
-    <Counter />
+    <InputSample />
 
   );
 }
--- react-practice-2/begin-react/src/InputSample.js
+++ react-practice-2/begin-react/src/InputSample.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 function InputSample(){
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    };
+
     return (
         <div>
             <input />
             <button>초기화</button>
+            <div>
+                <b>값: </b>
+            </div>
         </div>
-    )
+    );
 }
+
+export default InputSample;
