f2e886cb82c3929095e88e4f9801540771ce1590
jihye0 <llimjihye@gmail.com>
2023-07-24 11:26:55 +0900

    Until 9

--- react-practice-2/begin-react/src/InputSample.js
+++ react-practice-2/begin-react/src/InputSample.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function InputSample(){
-    const [text, setText] = useState('');
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const {name, nickname} = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+
+    const onReset = () => {
+        setInputs({
+            name: '',
+            nickname: '',
+        })
     };
 
     return (
         <div>
-            <input />
-            <button>초기화</button>
+            <input name="name" placeholder="이름" onChange={onChange} value={name}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
