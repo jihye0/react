3d085f8648c425d45b143eab63b60f67101ee942
jihye0 <llimjihye@gmail.com>
2023-07-25 14:57:49 +0900

    until 24

--- react-practice-2/begin-react/src/UserList.js
+++ react-practice-2/begin-react/src/UserList.js
@@ -1,39 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserDispatch } from './App';
 
-function User({ user }){
+const User = React.memo(function User({ user }){
+    const dispatch = useContext(UserDispatch);
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b 
+            style={{
+                cursor: 'pointer',
+                color: user.active ? 'green': 'black'
+            }}
+            onClick={()=>{
+                dispatch({type: 'TOGGLE_USER', id: user.id})
+            }}
+            >
+                {user.username}
+            </b>
+            &nbsp;
+            <span>({user.email})</span>
+            <button onClick={() => {
+                dispatch({type: 'REMOVE_USER', id: user.id});
+            }}>
+                삭제</button>
+        </div>
+    );
+});
+
+function UserList({users}){
+    return (
+        <div>
+            {users.map(user => (
+                <User user={user} key={user.id} />
+            ))}
         </div>
     );
 }
 
-function UserList(){
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id:3,
-            username: 'liz',
-            emial: 'liz@example.com'
-        }
-    ];
-
-    return (
-        <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
-        </div>
-    );
-}
-
-export default UserList;
+export default React.memo(UserList);
