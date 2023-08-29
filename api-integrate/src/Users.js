3cde6db3b8081b1de5c225ce1653896a65ff8aea
jihye0 <llimjihye@gmail.com>
2023-08-29 18:16:10 +0900

    router

---
+++ api-integrate/src/Users.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function Users(){
+    const [users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setError(null);
+                setUsers(null);
+                setLoading(true);
+                const response = await axios.get(
+                    'https://jsonplaceholder.typicode.com/users'
+                );
+                setUsers(response.data);
+            } catch (e) {
+                setError(e);
+            }
+            setLoading(false);
+        };
+        fetchUsers();
+    }, []);
+
+    if (loading) return <div></div>
+    if (error)
+    if (!users)
+
+
+
+}
---
+++ router-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  );
+};
+
+export default App;
